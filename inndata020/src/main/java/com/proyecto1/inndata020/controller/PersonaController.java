7a90a39c77617a4e177891fb56ae2f6b1c311da7
Brandon-Visoso <kyudaeu2099@gmail.com>
2026-02-28 06:00:24 -0600

    Puse el codigo controller

--- inndata020/src/main/java/com/proyecto1/inndata020/controller/PersonaController.java
+++ inndata020/src/main/java/com/proyecto1/inndata020/controller/PersonaController.java
@@ -15,4 +15,32 @@ public class PersonaController {
     @Autowired
     private IPersonaService personaService;
 
+    // SELECCIONA A TODAS LAS PERSONAS
+
+    @GetMapping("/listar")
+    public List<PersonaEntity> listarPersonas() {
+        return personaService.listarPersonas();
+    }
+
+    // BUSCA POR ID
+
+    @GetMapping("/buscar/{id}")
+    public PersonaEntity buscarPorId(@PathVariable Integer id) {
+        return personaService.buscarPorId(id);
+    }
+
+    // GUARDA UNA NUEVA PERSONA
+
+    @PostMapping("/guardar")
+    public PersonaEntity guardarPersona(@RequestBody PersonaEntity persona) {
+        return personaService.guardarPersona(persona);
+    }
+
+    // ACTUALIZA UNA PERSONA EXISTENTE
+
+    @PutMapping("/actualizar/{id}")
+    public PersonaEntity actualizarPersona(@PathVariable Integer id, @RequestBody PersonaEntity persona) {
+        return personaService.actualizarPersona(id, persona);
+    }
+
 }
